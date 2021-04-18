2d3616ac9888006d4afc6eb30ee555ac2456fc6a
martin-wachira <martinnwachira@gmail.com>
2021-04-18 14:03:45 +0300

    step 3

--- src/components/counter/counter-index.js
+++ src/components/counter/counter-index.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
+    const [counter, setCounter] = useState(5);
   return (
     <div>
-      <button>Counter</button>
+      <button onClick={()=>setCounter(counter*2)}>{counter}</button>
     </div>
   );
 };
 
-export default Counter
+export default Counter;
